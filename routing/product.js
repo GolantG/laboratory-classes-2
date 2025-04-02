d23a5d27ab8fe6a8337d75056ec35e1a341d473e
GolantG <halavan_aehit@students.vizja.pl>
2025-04-02 20:54:56 +0200

    try

--- routing/product.js
+++ routing/product.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
 const { STATUS_CODE } = require("../constants/statusCode");
 const renderNewProductPage = require("../views/renderNewProductPage");
 
